9734412e4d45ccb4810d902c4d06d039c861a70f
Drew Carpenter <141358445+CarpenterDubVee@users.noreply.github.com>
2023-08-07 21:59:23 -0400

    Update GetSites-NoSecStack.py

--- GetSites-NoSecStack.py
+++ GetSites-NoSecStack.py
@@ -12,5 +12,5 @@ GET_Sites = cgx_session1.get.sites()
 if GET_Sites.cgx_status:
     sitelist = GET_Sites.cgx_content.get("items", None)
     for site in sitelist:
-        if site["security_policysetstack_id"] == None:
+        if site["security_policysetstack_id"] == None and site["element_cluster_role"] == "SPOKE":
             print (site["name"], site["security_policysetstack_id"])
