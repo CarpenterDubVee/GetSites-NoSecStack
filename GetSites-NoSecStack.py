c8c58588f7114b66ecf894c8da5165c17b2322c4
Drew Carpenter <141358445+CarpenterDubVee@users.noreply.github.com>
2023-08-07 21:47:56 -0400

    Create GetSites-NoSecStack.py

---
+++ GetSites-NoSecStack.py
@@ -0,0 +1,16 @@
+import cloudgenix
+
+AUTH_TOKEN='EnterAuth Token'
+
+#Instantiate Cloudgenix API object
+cgx_session1 = cloudgenix.API("https://api.elcapitan.cloudgenix.com", ssl_verify=False)
+cgx_session1.interactive.use_token(AUTH_TOKEN)
+
+####Get the Site List Config
+GET_Sites = cgx_session1.get.sites()
+
+if GET_Sites.cgx_status:
+    sitelist = GET_Sites.cgx_content.get("items", None)
+    for site in sitelist:
+        if site["security_policysetstack_id"] == None:
+            print (site["name"], site["security_policysetstack_id"])
